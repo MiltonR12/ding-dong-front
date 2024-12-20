c74b8398ec2e95cdea71fd18fbe42efef4733858
SrLLamaDev <srllamadev@gmail.com>
2024-12-20 12:47:58 -0400

    el commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './page/home/HomePage';
+import ProductsPage from './page/products/ProductsPage';
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage/>} />
     </Routes>
   )
 }
